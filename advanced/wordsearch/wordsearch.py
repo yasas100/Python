29391a05d881f527c7c2af4e32368da418999540
temtamre <temtamre2012@gmail.com>
2018-06-07 10:43:00 -0600

    Stuck on column printing bug

--- advanced/wordsearch/wordsearch.py
+++ advanced/wordsearch/wordsearch.py
@@ -9,12 +9,16 @@ Concepts covered: Classes, 2D lists, file IO
 
 Sources:
 https://stackoverflow.com/questions/3739909/how-to-strip-all-whitespace-from-string
+
+BUG: Rows are printing in all columns
 """
 
 from random import *
 import sys
 
-ALPHABET = ["abcdefghijklmnopqrstuvwxyz"]
+ALPHABET = "abcdefghijklmnopqrstuvwxyz"
+
+
 def main():
     cw = Crossword()
     cw.generate_crossword()
@@ -24,59 +28,63 @@ class Crossword:
     def __init__(self, filename="wordlist.txt"):
         self.length = 10
         self.width  = 10
-        self.grid   = [["0"] * 10] * 10
+        self.grid   = [["."] * 10] * 10
 
-        self.hCount = 3
-        self.vCount = 3
-        self.dCount = 3
+        self.hCount = 2
+        self.vCount = 2
+        self.dCount = 2
 
         self.wordlist = self.getWords(filename)
+        self.included_words = []
         self.errorCheck()
 
     # Return: The crossword puzzle to be used
     def generate_crossword(self):
-        print("Grid:", type(self.grid), "of", type(self.grid[0]))
-
         # Insert all the words to be used in their proper orientations
         self.insert_horizontal()
-        self.insert_vertical()
-        self.insert_diagonal()
+        #self.insert_vertical()
+        #self.insert_diagonal()
 
         # Fill the rest
         for i in range(self.length):
             for j in range(self.width):
-                if self.grid[i][j] == "0":
+                if self.grid[i][j] == ".":
                     randIndex = randint(0,25)
-                    self.grid[i][j] = ALPHABET[randIndex]
+                    self.grid[i][j] = str(i) + str(j)
 
     def insert_horizontal(self):
         for i in range(self.hCount):
             word = self.wordlist.pop()
+            self.included_words.append(word)
             x = randint(0,10-len(word))
             y = randint(0,9)
 
+            print(word, x, y, end="\n\n")
             for letter in word:
-                self.grid[x][y] = letter
+                self.grid[y][x] = letter.upper()
                 x += 1
+        
 
     def insert_vertical(self):
         for i in range(self.vCount):
             word = self.wordlist.pop()
+            self.included_words.append(word)
             x = randint(0,9)
             y = randint(0,10-len(word))
 
             for letter in word:
-                self.grid[x][y] = letter
+                self.grid[y][x] = letter.upper()
                 y += 1
 
     def insert_diagonal(self):
         for i in range(self.dCount):
             word = self.wordlist.pop()
+            self.included_words.append(word)
             x = randint(0,10-len(word))
             y = randint(0,10-len(word))
 
             for letter in word:
-                self.grid[x][y] = letter
+                self.grid[y][x] = letter.upper()
                 x += 1
                 y += 1
 
@@ -95,9 +103,9 @@ class Crossword:
 
     # Prints the grid in it's current state
     def print_grid(self):
-        for i in self.length:
-            for j in self.height:
-                print(grid[i][j], end=" ")
+        for i in range(self.length):
+            for j in range(self.width):
+                print(self.grid[i][j], end=" ")
             print()
 
     def errorCheck(self):
